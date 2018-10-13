2580d1932d63f4152323f486bae69c043f88445a
Logan Mahan <mahan@royal-05.cs.wisc.edu>
2018-10-12 20:51:02 -0500

    Added a while loop to each method that should allow for the queues to enqueue and dequeue all contents. Now that I think about it, we should copy the block counts from one struct to another, that way we can keep track of the total amount by the time the printStats method is called. One error I am running across is that the Queue is defined as an incomplete struct. Tried to figure this out but didn't have enough time to resolve the error. Fingers crossed that the while loops work, they should.

--- main.c
+++ main.c
@@ -6,57 +6,83 @@
 #include <ctype.h>
 
 int queueSize = 10;
-int buffSize = 200;
+int buffSize = 20;
 Queue* initial;
 Queue* intermediate;
 Queue* final;
+
+void* reader();
+void* munch1();
+void* munch2();
+void* writer();
 
 int main(){
 	initial = createStringQueue(queueSize);
 	intermediate = createStringQueue(queueSize);
 	final = createStringQueue(queueSize);
-
+	reader();
+	munch1();
+	munch2();
+	writer();
 	// create reader thread
+	printQueueStats(final);
 }
 // create reader thread and send std input
 
 void *reader(){
-	char buff[200];
-	int count = read(0, buff, buffSize);
-	while(count > 0){
-		if(count < buffSize){
-			char* line = malloc(1 + sizeof(char) * count); 
-			strncpy(line, buff, count);
-			*(line+count) = '\0';
+	char buff[buffSize];
+	while(fgets(buff, buffSize, stdin) != NULL){
+		printf("%s\n", buff);
+		if((strlen(buff) < (size_t)buffSize) && (buff[buffSize] == '\0')){
+			char* line = malloc(1 + strlen(buff));
+			strncpy(line, buff, strlen(buff));
+//			printf("%s\n", line);
 			enqueueString(initial, line);
 		}
-		count = read(0, buff, buffSize);
+			
 	}
+	return NULL;
 
 }
 
+// make sure enqueue count = dequeue count for new struct
+
 void *munch1(){
-	char* string = dequeueString(initial);
-	char* temp = string;
+	char* string;
+	char* temp;
+	while((initial->enqueueCount) > (intermediate->enqueueCount)){
+	string = dequeueString(initial);
+	temp = string;
 	while((temp = strchr(temp, ' ')) != NULL){
 		*(temp) = '*';
 		temp++;
 	}
 	enqueueString(intermediate, string);
+	}
+	return NULL;
 }
 
 void *munch2(){
-	char* string = dequeueString(intermediate);
+	char* string;
+	while((intermediate->enqueueCount) > (final->enqueueCount)){
+	string = dequeueString(intermediate);
 	for(int i = 0; i < buffSize; i++){
 		if(string[i] >= 'a' && string[i] <= 'z'){
 			string[i] = toupper(string[i]);
 		}
 	}
 	enqueueString(final, string);
+	}
+	return NULL;
 }
 
-void *writer(){
-	char *print = dequeueString(final);
-	fprintf(stdout, "%s\n", print);
 
+void *writer(){
+	char* print;
+	while((final->enqueueCount) > (final->dequeueCount)){
+	print = dequeueString(final);
+	// print out all contents of the queue
+	fprintf(stdout, "%s\n", print);
+	}
+	return NULL;
 }
