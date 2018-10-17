349071cfef80ed4ce1ec31cfd227542de67d6e01
Logan Mahan <mahan@snares-06.cs.wisc.edu>
2018-10-17 12:14:42 -0500

    Updated main

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include <pthread.h>
 
 int queueSize = 10;
-int buffSize = 20;
+int buffSize = 1024;
 Queue* initial;
 Queue* intermediate;
 Queue* final;
@@ -32,13 +32,29 @@ int main(){
 
 void *reader(){
 	char buff[buffSize];
-	while(fgets(buff, buffSize, stdin) != NULL){
+//	fgetc, check for null pointer, end of line \n, EOF,  or if there is no character (empty file)
+//	What about
+//	Run against an empty file with no end line
+	int i = 0;
+	char c;
+	while((c = fgetc(stdin)) != EOF){
 //		printf("%s\n", buff);
-		if((strlen(buff) < (size_t)buffSize) && (buff[buffSize] == '\0')){
-			char* line = malloc(1 + strlen(buff));
-			strncpy(line, buff, strlen(buff));
-//			printf("%s\n", line);
-			enqueueString(initial, line);
+//		if((strlen(buff) < (size_t)buffSize) && (buff[buffSize] == '\0')){
+		if(i < buffSize-1){
+
+			if(c == '\n'){
+				buff[i] = '\0';
+				char* line = malloc(sizeof(char)*i);
+//				char* line = malloc(1 + strlen(buff));
+				strncpy(line, buff, strlen(buff));
+//				printf("%s\n", line);
+				enqueueString(initial, line);
+				i = 0;
+			}
+			buff[i] = c;
+			i++;
+		}else if((i > buffSize-1) && (c == '\n')){
+			printf("Error: Input exceeded buffer size.");
 		}
 			
 	}
@@ -52,13 +68,13 @@ void *munch1(){
 	char* string;
 	char* temp;
 	while((initial->enqueueCount) > (intermediate->enqueueCount)){
-	string = dequeueString(initial);
-	temp = string;
-	while((temp = strchr(temp, ' ')) != NULL){
-		*(temp) = '*';
-		temp++;
-	}
-	enqueueString(intermediate, string);
+		string = dequeueString(initial);
+		temp = string;
+		while((temp = strchr(temp, ' ')) != NULL){
+			*(temp) = '*';
+			temp++;
+		}
+		enqueueString(intermediate, string);
 	}
 	return NULL;
 }
@@ -66,13 +82,13 @@ void *munch1(){
 void *munch2(){
 	char* string;
 	while((intermediate->enqueueCount) > (final->enqueueCount)){
-	string = dequeueString(intermediate);
-	for(int i = 0; i < buffSize; i++){
-		if(string[i] >= 'a' && string[i] <= 'z'){
-			string[i] = toupper(string[i]);
+		string = dequeueString(intermediate);
+		for(int i = 0; i < buffSize; i++){
+			if(string[i] >= 'a' && string[i] <= 'z'){
+				string[i] = toupper(string[i]);
+			}
 		}
-	}
-	enqueueString(final, string);
+		enqueueString(final, string);
 	}
 	return NULL;
 }
@@ -81,9 +97,9 @@ void *munch2(){
 void *writer(){
 	char* print;
 	while((final->enqueueCount) > (final->dequeueCount)){
-	print = dequeueString(final);
+		print = dequeueString(final);
 	// print out all contents of the queue
-	fprintf(stdout, "%s", print);
+		fprintf(stdout, "%s", print);
 	}
 	return NULL;
 }
