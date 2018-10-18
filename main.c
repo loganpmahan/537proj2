5730cf43a9d25e6c41b44265e721fa8302102955
Samuel Ware <sware@royal-15.cs.wisc.edu>
2018-10-17 20:27:44 -0500

    Fix small exit condition

--- main.c
+++ main.c
@@ -6,11 +6,14 @@
 #include <ctype.h>
 #include <pthread.h>
 
+typedef struct QueueHolder {
+    Queue* initial;
+    Queue* intermediate;
+    Queue* final;
+}QueueHolder;
+
 const int queueSize = 10;
 const int buffSize = 1024;
-Queue* initial;
-Queue* intermediate;
-Queue* final;
 
 void* reader();
 void* munch1();
@@ -18,48 +21,53 @@ void* munch2();
 void* writer();
 
 int main(){
-	initial = createStringQueue(queueSize);
-	intermediate = createStringQueue(queueSize);
-	final = createStringQueue(queueSize);
+    QueueHolder *qh = (QueueHolder*) malloc(sizeof(QueueHolder));
+    if (qh == NULL) {
+        fprintf(stderr, "Out of memory.\n");
+        exit(1);
+    }
+	qh->initial = createStringQueue(queueSize);
+	qh->intermediate = createStringQueue(queueSize);
+	qh->final = createStringQueue(queueSize);
     
     pthread_t reader_t, munch1_t, munch2_t, writer_t;
     
-    pthread_create(&reader_t, NULL, reader, NULL);
-    pthread_create(&munch1_t, NULL, munch1, NULL);
-    pthread_create(&munch2_t, NULL, munch2, NULL);
-    pthread_create(&writer_t, NULL, writer, NULL);
+    pthread_create(&reader_t, NULL, reader, (void*) qh);
+    pthread_create(&munch1_t, NULL, munch1, (void*) qh);
+    pthread_create(&munch2_t, NULL, munch2, (void*) qh);
+    pthread_create(&writer_t, NULL, writer, (void*) qh);
 
     pthread_join(reader_t, NULL);
     pthread_join(munch1_t, NULL);
     pthread_join(munch2_t, NULL);
-    pthread_join(writer_t, NULL);
+    pthread_join(writer_t, NULL);    
 
-    printQueueStats(initial);
-    printQueueStats(intermediate);
-	printQueueStats(final);
+    printQueueStats(qh->initial);
+    printQueueStats(qh->intermediate);
+	printQueueStats(qh->final);
 }
 // create reader thread and send std input
 
-void *reader(){
-//	fgetc, check for null pointer, end of line \n, EOF,  or if there is no character (empty file)
-//	What about
-//	Run against an empty file with no end line
+void *reader(void* arg){
+    QueueHolder *qh = (QueueHolder*) arg;
 	int i = 0;
 	char c;
     char* buff = (char*) malloc(buffSize * sizeof(char));
     if(buff == NULL) {
         fprintf(stderr, "Out of Memory.\n");
-        exit(1);
+        enqueueString(qh->initial, NULL);
+        pthread_exit(0);
     }
 	while((c = fgetc(stdin)) != EOF){
 		if(i <= buffSize-1){
 			if(c == '\n'){
 				buff[i] = '\0';
-				enqueueString(initial, buff);
+				enqueueString(qh->initial, buff);
                 buff = (char*) malloc(buffSize * sizeof(char));
                 if (buff == NULL) {
                     fprintf(stderr, "Out of memory.\n");
-                    exit(1);
+                    enqueueString(qh->initial, NULL);
+                    pthread_exit(0);
                 }
 				i = 0;
 			} else {
@@ -73,54 +81,58 @@ void *reader(){
             buff = (char*) malloc(buffSize * sizeof(char));
             if (buff == NULL) {
                 fprintf(stderr, "Out of memory.\n");
-                exit(1);
+                enqueueString(qh->initial, NULL);
+                pthread_exit(0);
             }
 		}
 	}
     if (i != 0) {
         buff[i] = '\0';
-        enqueueString(initial, buff);
+        enqueueString(qh->initial, buff);
     } 
-    enqueueString(initial, NULL);
+    enqueueString(qh->initial, NULL);
 	pthread_exit(0);
 }
 
 // make sure enqueue count = dequeue count for new struct
 
-void *munch1(){
+void *munch1(void* arg){
+    QueueHolder *qh = (QueueHolder*) arg;
 	char* string;
 	char* temp;
 	do {
-		string = dequeueString(initial);
+		string = dequeueString(qh->initial);
 		temp = string;
 		while(string != NULL && (temp = strchr(temp, ' ')) != NULL){
 			*(temp) = '*';
 			temp++;
 		}
-		enqueueString(intermediate, string);
+		enqueueString(qh->intermediate, string);
 	} while (string != NULL);
 	pthread_exit(0);
 }
 
-void *munch2(){
+void *munch2(void* arg){
+    QueueHolder *qh = (QueueHolder*) arg;
 	char* string;
     do {
-		string = dequeueString(intermediate);
+		string = dequeueString(qh->intermediate);
 		for(int i = 0; i < buffSize && string != NULL; i++){
 			if(string[i] >= 'a' && string[i] <= 'z'){
 				string[i] = toupper(string[i]);
 			}
 		}
-		enqueueString(final, string);
+		enqueueString(qh->final, string);
 	} while (string != NULL);
 	pthread_exit(0);
 }
 
 
-void *writer(){
-	char* print;
+void *writer(void* arg){
+	QueueHolder *qh = (QueueHolder*) arg;
+    char* print;
 	do {
-		print = dequeueString(final);
+		print = dequeueString(qh->final);
 	    if (print == NULL)
             break;
 		fprintf(stdout, "%s\n", print);
